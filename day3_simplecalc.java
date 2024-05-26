c7a9d8761ca6a9e85db5568fd3de1a73adedc907
Deepak Xettri <139208927+Dipakxettri@users.noreply.github.com>
2024-05-26 11:17:21 +0545

    Create day3_simplecalc.java
    This code presents a basic calculator program for day 3.

---
+++ day3_simplecalc.java
@@ -0,0 +1,11 @@
+// Day 3 simple calculator program
+public class day3_simplecalc {
+    
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+    
+}
