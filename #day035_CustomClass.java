240661ff7272fa001fcde4de29c9086fbe7a47dc
Deepak Ghimire <139208927+Dipakxettri@users.noreply.github.com>
2024-07-09 14:27:11 +0545

    Create #day035_CustomClass.java
    Day 35 : This file includes a first custom class in Java.

---
+++ #day035_CustomClass.java
@@ -0,0 +1,17 @@
+
+//Custom Class 
+
+class Employee{
+    String name;
+    int salary;
+}
+
+public class CustomClass {
+    public static void main(String[] args) {
+        Employee dg = new Employee();
+        dg.name = "  Deepak Ghimire";
+        dg.salary = 80;
+        System.out.println(dg.name);
+        System.out.println(dg.salary);
+    }
+}
